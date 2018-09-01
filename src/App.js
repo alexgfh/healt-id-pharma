e3d1e7935161a27ad8b79b0a65c9b8ec0a1d9909
Alex <alexgfh@gmail.com>
2018-09-02 00:19:47 +0800

    Change login to access

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => (
   <Router>
     <div>
       <hr />
-      <Route exact path="/login/*" component={Login} />
+      <Route exact path="/access/*" component={Login} />
       <Route path="/about" component={About} />
       <Route path="/medications" component={Medications} />
     </div>
